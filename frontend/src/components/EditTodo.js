b5f9ba1e68ebd82fe0afbde63ee2089a72b2876c
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-11-27 02:51:12 +0500

    added edit todo

--- frontend/src/components/EditTodo.js
+++ frontend/src/components/EditTodo.js
@@ -1,5 +1,22 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
 
-function editTodo({todo}){
-    const
+function EditTodo({todo}) {
+  const [text, setText] = useState(todo.text);
+
+  const handleUpdateTodo = () => {
+    // Implement API call to update the ToDo item here
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button onClick={handleUpdateTodo}>Update</button>
+    </div>
+  );
 }
+
+export default EditTodo;
